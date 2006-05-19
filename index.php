d447176cff09531a464fae7a08d1cdf59274dd8f
xiphux <xiphux@centraldogma.(none)>
2006-05-19 16:28:17 -0400

    Start adding index and function library

---
+++ index.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ *  index.php
+ *  MDB: A media database
+ *  Component: Index page
+ *
+ *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+ /*
+  * Start output buffering
+  */
+ ob_start();
+
+ /*
+  * Version definitions
+  */
+ $version = "v01a";
+ $mdb_appstring = "MDB $version";
+
+ /*
+  * Database connection
+  */
+ include_once('db.inc.php');
+
+ /*
+  * Instantiate Smarty
+  */
+ include_once($mdb_conf['smarty_prefix'] . "Smarty.class.php");
+ $tpl =& new Smarty;
+ $tpl->caching = $mdb_conf['smarty_caching'];
+ $tpl->cache_lifetime = $mdb_conf['smarty_cache_lifetime'];
+ $tpl->load_filter('output','trimwhitespace');
+
+ ob_start();
+ if (isset($_GET['u'])) {
+ 	switch ($_GET['u']) {
+		case "updatedb":
+			exec("php updatedb.php 2>/dev/null >&- <&- >/dev/null &");
+			echo "Database updating";
+			break;
+		default:
+			echo "404";
+			break;
+	}
+ } else {
+ }
+ $main = ob_get_contents();
+ ob_end_clean();
+
+ $tpl->clear_all_assign();
+ $title = $mdb_conf['title'];
+ $tpl->assign("title",$title);
+ $tpl->display("header.tpl");
+
+ echo $main;
+
+ $tpl->display("footer.tpl");
+
+ ob_end_flush();
+?>
