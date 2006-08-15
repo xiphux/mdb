c988eb730eda7ae123fb17c9d3dfa1fdce08b9b4
xiphux <xiphux@centraldogma.(none)>
2006-08-15 06:20:53 -0400

    Update sql file

--- mdb.sql
+++ mdb.sql
@@ -1,13 +1,40 @@
 -- phpMyAdmin SQL Dump
--- version 2.8.0.2
+-- version 2.8.2.1
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Jun 11, 2006 at 09:29 PM
--- Server version: 5.0.21
--- PHP Version: 5.1.4-pl1-gentoo
+-- Generation Time: Aug 15, 2006 at 06:19 AM
+-- Server version: 5.0.22
+-- PHP Version: 5.1.4-pl6-gentoo
 -- 
 -- Database: `mdb`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `animenfo`
+-- 
+-- Creation: Jun 26, 2006 at 03:36 PM
+-- Last update: Aug 15, 2006 at 05:38 AM
+-- Last check: Aug 01, 2006 at 04:58 AM
+-- 
+
+CREATE TABLE `animenfo` (
+  `id` int(11) NOT NULL auto_increment,
+  `title_id` int(11) NOT NULL,
+  `name` varchar(255) default NULL,
+  `nfo_type` varchar(255) NOT NULL,
+  `nfo_id` varchar(255) NOT NULL,
+  `nfo_n` varchar(255) NOT NULL,
+  `nfo_t` varchar(255) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- 
+-- RELATIONS FOR TABLE `animenfo`:
+--   `title_id`
+--       `titles` -> `id`
 -- 
 
 -- --------------------------------------------------------
@@ -16,7 +43,8 @@
 -- Table structure for table `file_title`
 -- 
 -- Creation: Jun 05, 2006 at 06:08 PM
--- Last update: Jun 11, 2006 at 04:26 AM
+-- Last update: Aug 14, 2006 at 07:01 PM
+-- Last check: Aug 01, 2006 at 04:58 AM
 -- 
 
 CREATE TABLE `file_title` (
@@ -39,7 +67,8 @@ CREATE TABLE `file_title` (
 -- Table structure for table `files`
 -- 
 -- Creation: Jun 05, 2006 at 06:08 PM
--- Last update: Jun 11, 2006 at 04:25 AM
+-- Last update: Aug 14, 2006 at 07:00 PM
+-- Last check: Aug 01, 2006 at 04:58 AM
 -- 
 
 CREATE TABLE `files` (
@@ -49,7 +78,7 @@ CREATE TABLE `files` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `file` (`file`),
   KEY `size` (`size`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=16861 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -57,8 +86,8 @@ CREATE TABLE `files` (
 -- Table structure for table `tags`
 -- 
 -- Creation: Jun 10, 2006 at 10:01 PM
--- Last update: Jun 11, 2006 at 12:57 AM
--- Last check: Jun 10, 2006 at 10:01 PM
+-- Last update: Aug 14, 2006 at 07:31 PM
+-- Last check: Aug 01, 2006 at 04:58 AM
 -- 
 
 CREATE TABLE `tags` (
@@ -66,20 +95,21 @@ CREATE TABLE `tags` (
   `tag` varchar(255) NOT NULL,
   PRIMARY KEY  (`id`),
   KEY `tag` (`tag`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 -- 
 -- Table structure for table `title_tag`
 -- 
--- Creation: Jun 10, 2006 at 10:04 PM
--- Last update: Jun 11, 2006 at 09:21 PM
+-- Creation: Aug 15, 2006 at 06:02 AM
+-- Last update: Aug 15, 2006 at 06:05 AM
 -- 
 
 CREATE TABLE `title_tag` (
   `title_id` int(11) NOT NULL,
-  `tag_id` int(11) NOT NULL
+  `tag_id` int(11) NOT NULL,
+  UNIQUE KEY `title_id` (`title_id`,`tag_id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -88,7 +118,8 @@ CREATE TABLE `title_tag` (
 -- Table structure for table `titles`
 -- 
 -- Creation: Jun 05, 2006 at 06:08 PM
--- Last update: Jun 11, 2006 at 04:22 AM
+-- Last update: Aug 14, 2006 at 06:57 PM
+-- Last check: Aug 01, 2006 at 04:58 AM
 -- 
 
 CREATE TABLE `titles` (
@@ -98,7 +129,7 @@ CREATE TABLE `titles` (
   PRIMARY KEY  (`id`),
   UNIQUE KEY `path` (`path`),
   KEY `title` (`title`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=455 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -106,7 +137,8 @@ CREATE TABLE `titles` (
 -- Table structure for table `users`
 -- 
 -- Creation: Jun 05, 2006 at 06:08 PM
--- Last update: Jun 05, 2006 at 06:10 PM
+-- Last update: Jun 26, 2006 at 03:08 PM
+-- Last check: Aug 01, 2006 at 04:58 AM
 -- 
 
 CREATE TABLE `users` (
@@ -115,4 +147,4 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `privilege` tinyint(4) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
