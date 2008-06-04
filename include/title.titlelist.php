78beef568eb1269bdcd6304530e6c61396925c7c
Chris Han <xiphux@gmail.com>
2008-06-04 00:27:49 -0400

    Try a different titlelist implementation

--- include/title.titlelist.php
+++ include/title.titlelist.php
@@ -10,7 +10,14 @@
 
  function titlelist()
  {
+ 	/*
+	 * Not sure which is worse:
+	 * 37 database queries
+	 * or
+	 * looping once through many many titles
+	 */
  	global $db,$tables;
+	/*
 	$letters = array("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 	$numbers = array("0","1","2","3","4","5","6","7","8","9");
 	foreach ($numbers as $i => $number) {
@@ -26,6 +33,11 @@
 	$temp = $db->GetArray("SELECT * FROM " . $tables['titles'] . " WHERE LEFT(UPPER(TRIM(title)),1) NOT IN ('" . implode("','",$letters) . "','" . implode("','",$numbers) . "') ORDER BY title");
 	if (sizeof($temp) > 0)
 		$titles['other'] = $temp;
+	*/
+	$titlelist = $db->GetArray("SELECT id, title FROM " . $tables['titles'] . " ORDER BY title");
+	foreach ($titlelist as $i => $title) {
+		$titles[substr(strtolower(trim($titlelist[$i]["title"])),0,1)][] = $titlelist[$i];
+	}
 	return $titles;
  }
 
