9748d816d2645c77e03c4b7a7439099554d51907
Chris Han <xiphux@gmail.com>
2008-06-15 18:35:47 -0400

    Make a note about the query to iteration ratio in the source file

--- include/title.titlelist.php
+++ include/title.titlelist.php
@@ -8,6 +8,34 @@
  *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
  */
 
+ /*
+  * These constants define which operation to perform to
+  * enumerate the titlelist.
+  * One solution is to have one select for every letter
+  * in the alphabet and every number, and one for
+  * non-alphanumerics, yielding 37 selects.
+  * The other solution is to select all and iterate
+  * through everything.
+  * The speed of the iteration will depend on how many
+  * titles are in the list; iterating through 100 titles
+  * is very quick, but iterating through 100,000 titles
+  * is slow.  On the other hand, using multiple selects
+  * is always fixed at 37 selects and has an execution
+  * time independent of the number of titles.
+  * Therefore the system will get the title count, and
+  * choose the proper solution.  If the title count is
+  * below the QUERY_TO_ITERATION_RATIO * SQL_QUERIES,
+  * it will iterate, otherwise it will select.
+  * From my testing and calculations, on my machine with
+  * an optimal setup (mysql on the same server as php,
+  * php running eaccelerator), it was estimated that the
+  * iteration would be slower than the selects when the
+  * title count surpassed about 1850, therefore the ratio
+  * is set at 50 (37 * 50 = 1850).  If your setup is not
+  * as optimal (mysql on a slower separate database server,
+  * php not running eaccelerator, etc), you may want to
+  * adjust this ratio.
+  */
  define('QUERY_TO_ITERATION_RATIO',50);
  define('SQL_QUERIES',37);
 
