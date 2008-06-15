917b90449d4315556e39ff35af478104275b63e7
Chris Han <xiphux@gmail.com>
2008-06-15 01:33:33 -0400

    Add warning on main page when database is updating

--- include/display.mainpage.php
+++ include/display.mainpage.php
@@ -10,13 +10,21 @@
 
 function mainpage()
 {
-	global $tpl,$mdb_appstring,$db,$tables;
+	global $tpl,$mdb_appstring,$db,$tables,$mdb_conf;
 	$tpl->clear_all_assign();
 	$tpl->assign("banner",$mdb_appstring);
 	$tpl->assign("titlecount",$db->GetOne("SELECT COUNT(id) FROM " . $tables['titles']));
 	$tpl->assign("filecount",$db->GetOne("SELECT COUNT(id) FROM " . $tables['files']));
 	$tpl->assign("size",$db->GetOne("SELECT SUM(size) FROM " . $tables['files']));
 	$update = $db->GetOne("SELECT MAX(time) FROM " . $tables['dbupdate'] . " WHERE progress=0");
+	if ($mdb_conf['dbmutex']) {
+		$status = $db->GetOne("SELECT MAX(progress) FROM " . $tables['dbupdate']);
+		if ($status && $status > 0)
+			$tpl->assign("updating",TRUE);
+	} else {
+		if (shell_exec("ps ax | grep -v 'grep' | grep -c '" . $mdb_conf['phpexec'] . " include/updatedb.php'") >= 1)
+			$tpl->assign("updating",TRUE);
+	}
 	if ($update)
 		$tpl->assign("update",$update);
 	date_default_timezone_set("UTC");
--- templates/main.tpl
+++ templates/main.tpl
@@ -6,4 +6,4 @@
  *  Copyright (C) 2006 Christopher Han <xiphux@gmail.com>
  *}
  <p>{$banner}</p>
- <p><span class="bold">Titles:</span> {$titlecount}<br /><span class="bold">Files:</span> {$filecount}<br /><span class="bold">Total size of all files in database:</span> <span title="{$size}">{$size|size}</span><br /><span class="bold">Last database update:</span> {if $update}{$update|date_format:"%B %e, %Y %T"}{else}Never{/if}</p>
+ <p><span class="bold">Titles:</span> {$titlecount}<br /><span class="bold">Files:</span> {$filecount}<br /><span class="bold">Total size of all files in database:</span> <span title="{$size}">{$size|size}</span><br /><span class="bold">Last database update:</span> {if $update}{$update|date_format:"%B %e, %Y %T"}{else}Never{/if}<br />{if $updating}<span class="bold warning">Database currently updating</span>{/if}</p>
