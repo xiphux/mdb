93983340c9a0c836ef7fb6876275827ea7aa8f77
Chris Han <xiphux@gmail.com>
2008-07-10 21:09:03 -0400

    Cache title filelist

--- include/display.title.php
+++ include/display.title.php
@@ -14,14 +14,30 @@ include_once('include/tag.taglist.php');
 function title($tid)
 {
 	global $mdb_conf,$tpl;
+
+	$key = "title_" . $tid;
+	if (isset($_SESSION[$mdb_conf['session_key']]['user'])) {
+		$key .= "_user";
+		if ($mdb_conf['download'])
+			$key .= "_dl";
+	}
+	$out = mdb_memcache_get($key);
+	if ($out) {
+		echo $out;
+		return;
+	}
+
 	$title = titleinfo($tid);
+
+	$out = "";
+
 	$tpl->clear_all_assign();
 	$tpl->assign("title", $title);
 	if (isset($_SESSION[$mdb_conf['session_key']]['user']))
  		$tpl->assign("user",$_SESSION[$mdb_conf['session_key']]['user']);
 	$tpl->assign("taglist",taglist());
-	$tpl->display("title.tpl");
-	$tpl->display("fileliststart.tpl");
+	$out .= $tpl->fetch("title.tpl");
+	$out .= $tpl->fetch("fileliststart.tpl");
 
 	if (count($title['files']) > 0) {
 		$odd = TRUE;
@@ -44,7 +60,7 @@ function title($tid)
 				if (count($currentdir) > 1);
 					$tpl->assign("itemid",preg_replace($allowed,"_",implode("/",array_slice($currentdir,0,count($currentdir)-1))));
 				$tpl->assign("subid",preg_replace($allowed,"_",implode("/",$currentdir)));
-				$tpl->display("filelistitem.tpl");
+				$out .= $tpl->fetch("filelistitem.tpl");
 				$tpl->clear_all_assign();
 				$odd = !$odd;
 			}
@@ -60,16 +76,20 @@ function title($tid)
 			$tpl->assign("fileid",$file['id']);
 			$tpl->assign("filename",$file['file']);
 			$tpl->assign("filesize",$file['size']);
-			$tpl->display("filelistitem.tpl");
+			$out .= $tpl->fetch("filelistitem.tpl");
 			$odd = !$odd;
 		}
 	} else {
 		$tpl->clear_all_assign();
 		$tpl->assign("empty",TRUE);
-		$tpl->display("filelistitem.tpl");
+		$out .= $tpl->fetch("filelistitem.tpl");
 	}
 
-	$tpl->display("filelistend.tpl");
+	$out .= $tpl->fetch("filelistend.tpl");
+
+	mdb_memcache_set($key, $out);
+
+	echo $out;
 }
 
 ?>
