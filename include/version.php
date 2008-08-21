ed98700399382f2cffacf3fda39eb65155d848a5
Chris Han <xiphux@gmail.com>
2008-08-21 14:29:39 -0400

    Bump version

--- include/version.php
+++ include/version.php
@@ -7,7 +7,7 @@
  *  Copyright (C) 2008 Christopher Han <xiphux@gmail.com>
  */
 
- define("MDB_VERSION", "0.0.7");
+ define("MDB_VERSION", "0.0.8");
  define("MDB_APPSTRING", "MDB " . MDB_VERSION);
 
 ?>
